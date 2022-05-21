9b3f64ec3b0c691497695f8b2e8e943bbe98d816
roman <sokolovskyy@live.ru>
2022-05-21 13:54:25 +0200

    recreated DB from scratch, integrated import-export admin functionalities, cleaned ingredients.csv for import

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     """User administration."""
     list_display = (
         'pk',
