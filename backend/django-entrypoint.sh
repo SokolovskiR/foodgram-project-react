f52be7a453aa745c8989f1fbcd70b05cd83f6875
roman <sokolovskyy@live.ru>
2022-05-26 14:51:59 +0200

    backend docker and entrypoint

--- backend/django-entrypoint.sh
+++ backend/django-entrypoint.sh
@@ -9,4 +9,7 @@ python manage.py makemigrations --noinput
 echo "migrate"
 python manage.py migrate --noinput
 
+echo "starting gunicorn server ..."
+gunicorn foodgram_app.wsgi:application --bind :8000 
+
 exec "$@"
